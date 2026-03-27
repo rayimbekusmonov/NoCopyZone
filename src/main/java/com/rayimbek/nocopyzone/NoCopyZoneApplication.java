7f26d8b59d7f4d6b55141331950f5450a8ecd1b7
Rayimbek <rakhimbekmaksudjonoich@gmail.com>
2026-03-27 15:44:18 +0500

    Initial commit

---
+++ src/main/java/com/rayimbek/nocopyzone/NoCopyZoneApplication.java
@@ -0,0 +1,13 @@
+package com.rayimbek.nocopyzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoCopyZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoCopyZoneApplication.class, args);
+    }
+
+}
